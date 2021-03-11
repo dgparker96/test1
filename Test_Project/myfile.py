55dc29a24f917a0987863d15e6d36e2331d292c7
dp <dalton.parker@carolinafh.com>
2021-03-11 00:18:53 -0500

    new py

---
+++ Test_Project/myfile.py
@@ -0,0 +1,14 @@
+mynum = 3
+mynum1 = 6
+print(mynum + mynum1)
+
+if (mynum < mynum1):
+    print("all is good in the hood")
+
+print("DDDD  PPPP ")
+print("D   D P   P")
+print("D   D PPPP ")
+print("D   D P    ")
+print("D   D P    ")
+print("D   D P    ")
+print("DDDD  P    ")
